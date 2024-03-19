2e34bc4586dadeefa2e6b708249674bb26673ec3
gramthanos <gramthanos@gmail.com>
2024-03-19 12:35:41 +0200

    changes

--- bin/ovpn_api_server.py
+++ bin/ovpn_api_server.py
@@ -41,47 +41,204 @@ def api_ping():
 @auth.login_required
 def api_genconfig():
 
+
+	#curl -X POST -H "Content-Type: application/json" -d '{"SERVER_URL": "udp://protons.ddns.net:1101", "ROUTES": "10.13.1.255/16", "SERVER": "10.13.1.255/25", "DISABLE_DEFROUTE": true, "DISABLE_PUSH_BLOCK_DNS": true, "CLIENT_TO_CLIENT": true}' -H 'Authorization: Bearer example_web_secret' https://192.168.2.51:8001/api/genconfig --insecure
+
 	# curl -X POST -H "Content-Type: application/json" -d '{"host": "udp://protons.ddns.net:1194"}' -H 'Authorization: Bearer example_web_secret' https://0.0.0.0:8080/api/genconfig --insecure
 	# curl -X POST -H "Content-Type: application/json" -d '{"host": "udp://protons.ddns.net:1194", "route": "10.10.0.0/16"}' -H 'Authorization: Bearer example_web_secret' https://0.0.0.0:8080/api/genconfig --insecure
 	# curl -X POST -H "Content-Type: application/json" -d '{"host": "udp://protons.ddns.net:1194", "route": "10.10.0.0/16", "disableDefaultRoot": true, "dontPushDNS": true}' -H 'Authorization: Bearer example_web_secret' https://0.0.0.0:8080/api/genconfig --insecure
 
 	shell_command = ['ovpn_genconfig']
 
-	# Check paramters e.g. udp://IP_ADDRESS:3000
-	if not ("host" in request.json) or (not (type(request.json['host']) is str)):
-		return jsonResponse({"error": "Bad Request"}, 500)
-	else:
-		shell_command.append('-u')
-		shell_command.append(request.json['host'])
+#	# Check paramters e.g. udp://IP_ADDRESS:3000
+#	if not ("host" in request.json) or (not (type(request.json['host']) is str)):
+#		return jsonResponse({"error": "Bad Request"}, 500)
+#	else:
+#		shell_command.append('-u')
+#		shell_command.append(request.json['host'])
+#
+#	if ("route" in request.json):
+#		if type(request.json['route']) is str:
+#			shell_command.append('-r')
+#			shell_command.append(request.json['route'])
+#		elif type(request.json['route']) is list:
+#			request.json['route'] = list(filter(lambda x: (type(x) is str), request.json['route']))
+#			if len(request.json['route']) > 0:
+#				shell_command.append('-r')
+#				for r in request.json['route']:
+#					shell_command.append(r)
+#
+#	if ("dns" in request.json) and (type(request.json['dns']) is str):
+#		if type(request.json['dns']) is str:
+#			shell_command.append('-n')
+#			shell_command.append(request.json['dns'])
+#		elif type(request.json['dns']) is list:
+#			request.json['dns'] = list(filter(lambda x: (type(x) is str), request.json['dns']))
+#			if len(request.json['dns']) > 0:
+#				shell_command.append('-n')
+#				for n in request.json['dns']:
+#					shell_command.append(n)
+#
+#	if ("disableDefaultRoot" in request.json) and (type(request.json['disableDefaultRoot']) is bool) and request.json['disableDefaultRoot']:
+#		shell_command.append('-d')
+#
+#	if ("dontPushDNS" in request.json) and (type(request.json['dontPushDNS']) is bool) and request.json['dontPushDNS']:
+#		shell_command.append('-D')
 
-	if ("route" in request.json):
-		if type(request.json['route']) is str:
-			shell_command.append('-r')
-			shell_command.append(request.json['route'])
-		elif type(request.json['route']) is list:
-			request.json['route'] = list(filter(lambda x: (type(x) is str), request.json['route']))
-			if len(request.json['route']) > 0:
-				shell_command.append('-r')
-				for r in request.json['route']:
-					shell_command.append(r)
 
-	if ("dns" in request.json) and (type(request.json['dns']) is str):
-		if type(request.json['dns']) is str:
-			shell_command.append('-n')
-			shell_command.append(request.json['dns'])
-		elif type(request.json['dns']) is list:
-			request.json['dns'] = list(filter(lambda x: (type(x) is str), request.json['dns']))
-			if len(request.json['dns']) > 0:
-				shell_command.append('-n')
-				for n in request.json['dns']:
-					shell_command.append(n)
+	# OVP_AUTH: Authenticate  packets with HMAC using the given message digest algorithm (auth).
+	if ('a' in request.json):
+		shell_command.append('-a')
+		shell_command.append(request.json['a'])
+	elif ('AUTH' in request.json):
+		shell_command.append('-a')
+		shell_command.append(request.json['AUTH'])
 
-	if ("disableDefaultRoot" in request.json) and (type(request.json['disableDefaultRoot']) is bool) and request.json['disableDefaultRoot']:
+	# OVPN_EXTRA_SERVER_CONFIG
+	if ('e' in request.json):
+		shell_command.append('-e')
+		shell_command.append(request.json['e'])
+	elif ('EXTRA_SERVER_CONFIG' in request.json):
+		shell_command.append('-e')
+		shell_command.append(request.json['EXTRA_SERVER_CONFIG'])
+
+	# OVPN_EXTRA_CLIENT_CONFIG
+	if ('E' in request.json):
+		shell_command.append('-E')
+		shell_command.append(request.json['E'])
+	elif ('EXTRA_CLIENT_CONFIG' in request.json):
+		shell_command.append('-E')
+		shell_command.append(request.json['EXTRA_CLIENT_CONFIG'])
+
+	# OVPN_CIPHER: A list of allowable TLS ciphers delimited by a colon (cipher).
+	if ('C' in request.json):
+		shell_command.append('-C')
+		shell_command.append(request.json['C'])
+	elif ('CIPHER' in request.json):
+		shell_command.append('-C')
+		shell_command.append(request.json['CIPHER'])
+
+	# OVPN_TLS_CIPHER: Encrypt packets with the given cipher algorithm instead of the default one (tls-cipher).
+	if ('T' in request.json):
+		shell_command.append('-T')
+		shell_command.append(request.json['T'])
+	elif ('TLS_CIPHER' in request.json):
+		shell_command.append('-T')
+		shell_command.append(request.json['TLS_CIPHER'])
+
+	# OVPN_ROUTES
+	if ('r' in request.json):
+		shell_command.append('-r')
+		shell_command.append(request.json['r'])
+	elif ('ROUTES' in request.json):
+		shell_command.append('-r')
+		shell_command.append(request.json['ROUTES'])
+
+	# OVPN_SERVER: SERVER_SUBNET
+	if ('s' in request.json):
+		shell_command.append('-s')
+		shell_command.append(request.json['s'])
+	elif ('SERVER' in request.json):
+		shell_command.append('-s')
+		shell_command.append(request.json['SERVER'])
+
+	# OVPN_DEFROUTE: Disable default route
+	if ('d' in request.json):
+		shell_command.append('-d')
+	elif ('DISABLE_DEFROUTE' in request.json):
 		shell_command.append('-d')
 
-	if ("dontPushDNS" in request.json) and (type(request.json['dontPushDNS']) is bool) and request.json['dontPushDNS']:
+	# OVPN_SERVER_URL: SERVER_PUBLIC_URL
+	if ('u' in request.json):
+		shell_command.append('-u')
+		shell_command.append(request.json['u'])
+	elif ('SERVER_URL' in request.json):
+		shell_command.append('-u')
+		shell_command.append(request.json['SERVER_URL'])
+
+	# OVPN_DISABLE_PUSH_BLOCK_DNS: Disable 'push block-outside-dns'
+	if ('b' in request.json):
+		shell_command.append('-b')
+	elif ('DISABLE_PUSH_BLOCK_DNS' in request.json):
+		shell_command.append('-b')
+
+	# OVPN_CLIENT_TO_CLIENT: Enable client-to-client option
+	if ('c' in request.json):
+		shell_command.append('-c')
+	elif ('CLIENT_TO_CLIENT' in request.json):
+		shell_command.append('-c')
+
+	# OVPN_PUSH: PUSH
+	if ('p' in request.json):
+		shell_command.append('-p')
+		shell_command.append(request.json['p'])
+	elif ('PUSH' in request.json):
+		shell_command.append('-p')
+		shell_command.append(request.json['PUSH'])
+
+	# OVPN_DNS_SERVERS: DNS_SERVER
+	if ('n' in request.json):
+		shell_command.append('-n')
+		shell_command.append(request.json['n'])
+	elif ('DNS_SERVERS' in request.json):
+		shell_command.append('-n')
+		shell_command.append(request.json['DNS_SERVERS'])
+
+	# OVPN_DNS=0: Do not push dns servers
+	if ('D' in request.json):
 		shell_command.append('-D')
-		
+	elif ('DISABLE_DNS' in request.json):
+		shell_command.append('-D')
+
+	# OVPN_NAT=1: Configure NAT to access external server network
+	if ('N' in request.json):
+		shell_command.append('-N')
+	elif ('NAT' in request.json):
+		shell_command.append('-N')
+
+	# OVPN_KEEPALIVE: Set keepalive. Default: '10 60'
+	if ('k' in request.json):
+		shell_command.append('-k')
+		shell_command.append(request.json['k'])
+	elif ('KEEPALIVE' in request.json):
+		shell_command.append('-k')
+		shell_command.append(request.json['KEEPALIVE'])
+
+	# OVPN_MTU: Set client MTU
+	if ('m' in request.json):
+		shell_command.append('-m')
+		shell_command.append(request.json['m'])
+	elif ('MTU' in request.json):
+		shell_command.append('-m')
+		shell_command.append(request.json['MTU'])
+
+	# OVPN_DEVICE="tap": Use TAP device (instead of TUN device)
+	if ('t' in request.json):
+		shell_command.append('-t')
+	elif ('TAP' in request.json):
+		shell_command.append('-t')
+
+	# OVPN_COMP_LZO: Enable comp-lzo compression.
+	if ('z' in request.json):
+		shell_command.append('-z')
+	elif ('COMP_LZO' in request.json):
+		shell_command.append('-z')
+
+	# OVPN_OTP_AUTH: Enable two factor authentication using Google Authenticator.
+	if ('2' in request.json):
+		shell_command.append('-2')
+	elif ('OTP_AUTH' in request.json):
+		shell_command.append('-2')
+
+	# OVPN_FRAGMENT: FRAGMENT
+	if ('f' in request.json):
+		shell_command.append('-f')
+		shell_command.append(request.json['f'])
+	elif ('FRAGMENT' in request.json):
+		shell_command.append('-f')
+		shell_command.append(request.json['FRAGMENT'])
+
+
 
 	print(request.json)
 	result = subprocess.run(shell_command, capture_output=True, check=False)
